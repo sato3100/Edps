c0f98e6972c7ead6741502522175d6df78ab14c5
sato3100 <satoru4107@gmail.com>
2024-02-04 18:48:47 +0900

    分かりやすいように付け足した。

--- app/src/main/java/com/example/myedpsapplication/MainActivity.java
+++ app/src/main/java/com/example/myedpsapplication/MainActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    int x = 5;
+    if(x > 0){
+        System.out.println("こんにちは");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
