27a578d4385edc292794fb7f98575d1d5adcabc5
sato3100 <satoru4107@gmail.com>
2024-02-05 14:25:16 +0900

    もうわからん

--- app/src/main/java/com/example/myedpsapplication/MainActivity.java
+++ app/src/main/java/com/example/myedpsapplication/MainActivity.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
     int x = 5;
     if(x > 0){
         System.out.println("Windowsからテスト  ori");
         System.out.println("Macからテスト  ori");
         System.out.println("Macからテスト  tupi");
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
