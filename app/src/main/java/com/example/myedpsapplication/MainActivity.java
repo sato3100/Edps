7329d1371fbf971766d3653332bef79ea9ad509c
sato3100 <satoru4107@gmail.com>
2024-02-05 14:30:28 +0900

    とりあえずこれで良いはず

--- app/src/main/java/com/example/myedpsapplication/MainActivity.java
+++ app/src/main/java/com/example/myedpsapplication/MainActivity.java
@@ -1,22 +1,22 @@
 package com.example.myedpsapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-    int x = 5;
-    if(x > 0){
-        System.out.println("Windowsからテスト  ori");
-        System.out.println("Macからテスト  ori");
-        System.out.println("Macからテスト  tupi");
-    }
-
+    // このメソッドはActivityのライフサイクルには属さないため、ここに直接コードを記述することはできません。
+    // 代わりに、onCreateメソッド内や他の適切な場所にログ出力のコードを配置する必要があります。
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // ログを使用してメッセージを出力
+        Log.d("MainActivity", "Windowsからテスト  ori");
+        Log.d("MainActivity", "Macからテスト  ori");
+        Log.d("MainActivity", "Macからテスト  tupi");
     }
 }
