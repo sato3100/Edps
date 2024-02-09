a844ee96605f4731ecffed95e6a581629344cc96
checker888 <ort030627@gmail.com>
2024-02-10 00:16:46 +0900

    楽天apiから初めて返事が来た

--- app/src/main/java/com/example/myedpsapplication/Rakuten.java
+++ app/src/main/java/com/example/myedpsapplication/Rakuten.java
@@ -1,6 +1,7 @@
 package com.example.myedpsapplication;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,37 +13,64 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Rakuten extends AppCompatActivity {
 
-    private final String BASE_URL = "https://app.rakuten.co.jp/services/api/Recipe/CategoryLis\n" + "  t/20170426?\n" + "applicationId=[1087356199907491193]& categoryType=large";
-    private final String APPLICATION_ID = "f620061daa80949337fb24cebf3c94fe8398db64"; // あなたのAPIキーを設定
+   // private final String BASE_URL = "https://app.rakuten.co.jp/services/api/Recipe/CategoryLis\n" + "  t/20170426?\n" + "applicationId=[1087356199907491193]& categoryType=large";
+
+   // private final String BASE_URL = "https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?applicationId=1087356199907491193";
+  //  private  final  String  BASE_URL = "https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426/";
+   private  final  String  BASE_URL ="https://app.rakuten.co.jp/services/api/";
+    private final String APPLICATION_ID = "1087356199907491193"; // あなたのAPIキーを設定
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.sixth);
+        try{
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
+            RakutenRecipeApi api = retrofit.create(RakutenRecipeApi.class);
 
-        RakutenRecipeApi api = retrofit.create(RakutenRecipeApi.class);
+            Call<RecipeResponse> call = api.getRecipes(APPLICATION_ID, "10"); // カテゴリIDを設定
 
-        Call<RecipeResponse> call = api.getRecipes(APPLICATION_ID, "カテゴリID"); // カテゴリIDを設定
+            call.enqueue(new Callback<RecipeResponse>() {
+                @Override
+                public void onResponse(Call<RecipeResponse> call, Response<RecipeResponse> response) {
+                    TextView t = findViewById(R.id.textView4);
 
-        call.enqueue(new Callback<RecipeResponse>() {
-            @Override
-            public void onResponse(Call<RecipeResponse> call, Response<RecipeResponse> response) {
-                if (response.isSuccessful()) {
-                    // レスポンスを処理
-                    RecipeResponse recipeResponse = response.body();
-                    // ここでレシピ情報をUIに表示
+
+
+
+                    if (response.isSuccessful()) {
+                        // レスポンスを処理
+                        RecipeResponse recipeResponse = response.body();
+                        // ここでレシピ情報をUIに表示
+
+                        t.setText(recipeResponse.toString());
+                        
+
+
+                    }else{
+                       // t.setText("エラーはないけどなんかうまくいってない");
+                        t.setText(response.toString());
+                    }
                 }
-            }
 
-            @Override
-            public void onFailure(Call<RecipeResponse> call, Throwable t) {
-                // エラー処理
-            }
-        });
+                @Override
+                public void onFailure(Call<RecipeResponse> call, Throwable t) {
+                    // エラー処理
+                    TextView t2 = findViewById(R.id.textView4);
+                    t2.setText("エラった");
+                }
+            });
+        }catch (Exception e){
+            TextView t = findViewById(R.id.textView4);
+            t.setText(e.toString());
+        }
+
+
     }
 }
